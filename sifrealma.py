3197ae9945c7ab837bedfea48779f3726073602c
kilincmerve <99736353+kilincmerve@users.noreply.github.com>
2022-02-15 18:59:29 +0300

    Create sifrealma.py

---
+++ sifrealma.py
@@ -0,0 +1,8 @@
+isim = input("Lütfen kullanıcı adı girişi yapınız:")
+user_isim="merve"
+user_sifre="W@12"
+
+if isim == user_isim :
+  print(f"Merhaba {isim} ! Şifre : {user_sifre}")
+else:
+  print(f"Merhaba {isim} ! Sonra görüşürüz")
